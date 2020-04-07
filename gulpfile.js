fb3d3903ac15e991e0beb96a265634ddf86a1fb1
Mahmoud <mahmoudhussien06@gmail.com>
2020-04-07 21:25:38 +0200

    end larg hero and wrapper...etc

--- gulpfile.js
+++ gulpfile.js
@@ -5,32 +5,39 @@ cssvars = require('postcss-simple-vars'),
 nested = require('postcss-nested'),
 cssimport = require('postcss-import'),
 browserSync = require('browser-sync').create(),
+mixins = require('postcss-mixins'),
 watch = require('gulp-watch');
 
 
 gulp.task('style', function(){
 	return gulp.src('./app/assets/src/sass/main.css')
-	.pipe(postcss([cssimport, cssvars, nested, autoprefixer]))
+	.pipe(postcss([cssimport, mixins, cssvars, nested, autoprefixer]))
+	.on('error', function(errorinfo){
+		console.log(errorinfo);
+		this.emit('end');
+	})
 	.pipe(gulp.dest('./app/assets/css'))
 	.pipe(browserSync.stream());
 });
 
 
-gulp.task('scripts', function(){
-	console.log('you make scripts changes');
+
+gulp.task('html', function(){
+    return gulp.src('./app/index.html')
+        .pipe(browserSync.reload());
 });
+
 
 gulp.task('watch', function(){
 
 	browserSync.init({
+		notify: false,
 		server: {
 			baseDir: 'app'
 		}
 	});
 
-	gulp.watch('./app/index.html', function(){
-		browserSync.reload();
-	});
+	gulp.watch('./app/index.html', gulp.series('html'));
 
 	gulp.watch('./app/assets/src/**/*.css', gulp.series('style'));
 
