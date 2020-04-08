a280c06f561af435e34221742cd685fdd066666b
Mahmoud <mahmoudhussien06@gmail.com>
2020-04-08 18:45:44 +0200

    merged testimonail

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,18 @@
 let gulp = require('gulp'),
-postcss = require('gulp-postcss'),
-autoprefixer = require('autoprefixer'),
+postCsss = require('gulp-postcss'),
+autoPreFixers = require('autoprefixer'),
 cssvars = require('postcss-simple-vars'),
 nested = require('postcss-nested'),
-cssimport = require('postcss-import'),
+cssImport = require('postcss-import'),
 browserSync = require('browser-sync').create(),
 mixins = require('postcss-mixins'),
+cssNano = require('cssnano'),
 watch = require('gulp-watch');
 
 
 gulp.task('style', function(){
 	return gulp.src('./app/assets/src/sass/main.css')
-	.pipe(postcss([cssimport, mixins, cssvars, nested, autoprefixer]))
+	.pipe(postCsss([cssImport, mixins, cssvars, nested, autoPreFixers("last 5 versions"), cssNano]))
 	.on('error', function(errorinfo){
 		console.log(errorinfo);
 		this.emit('end');
